15802c3cc119b5180371da7449a3a3152a19a939
René Schumacher <rene.schumacher@microsoft.com>
2025-01-14 12:53:31 +0100

    Update sample.js

--- javascript/sample.js
+++ javascript/sample.js
@@ -1,3 +1,6 @@
 // PAT for testing connectivity to Azure DevOps
 const azdo_pat = '8ErJGkKh5YhUmyGzhy2OIExXRqOGTkUQ2nEEImtnyXgVWuc2y45RJQQJ99BAACAAAAAAArahAAASAZDOx7oi';
 const basic_auth_header = 'Authorizaton: Basic cGF0OjhFckpHa0toNVloVW15R3poeTJPSUV4WFJxT0dUa1VRMm5FRUltdG55WGdWV3VjMnk0NVJKUVFKOTlCQUFDQUFBQUFBQXJvaEFBQVNBWkRPeDdvaQ==';
+
+// GH PAT for testing
+const gh_pat = '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';
